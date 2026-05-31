5d27f68a9bcd833f6078dbaff4a8fa4be17c5860
r0w4n <6553343+r0w4n@users.noreply.github.com>
2026-05-31 20:14:00 +0100

    fixed footer links

---
+++ src/app/site-footer.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import { SiteFooter } from "./site-footer";
+
+describe("site-footer", () => {
+  it("links to consolidated static pages", () => {
+    render(<SiteFooter />);
+
+    expect(screen.getByRole("link", { name: "Privacy" })).toHaveAttribute(
+      "href",
+      "./privacy/"
+    );
+    expect(screen.getByRole("link", { name: "Methodology" })).toHaveAttribute(
+      "href",
+      "./methodology/"
+    );
+    expect(screen.getByRole("link", { name: "About" })).toHaveAttribute(
+      "href",
+      "./about/"
+    );
+  });
+});
